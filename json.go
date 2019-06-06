3393afc7a26f03b5c86adf1f83db4014b8f72670
Elizabeth Wanjiku <lizloopjames@gmail.com>
2019-06-07 00:57:09 +0300

    first commit

---
+++ json.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Address  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v Address)", p.Name, p.Address)
+}
+
+func main() {
+	a := Person{"Mathew Stevey", 42}
+	fmt.Println(a)
+}
+
+
+
+
